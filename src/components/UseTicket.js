9a303c77ebc1d5c40fed50bb3aa15e0378f09cf9
KieschDo <dk108@hdm-stuttgart.de>
2021-06-15 10:34:59 +0200

    Ticketroom Changes

--- src/components/UseTicket.js
+++ src/components/UseTicket.js
@@ -4,6 +4,9 @@ import Web3 from 'web3';
 import Ticket from '../abis/Ticket.json';
 import MainFeaturedPost from './Card2.js'
 
+import Countdown from './Countdown2.js'
+import test from '../test.gif';
+
 const mainFeaturedPost = {
     title: 'Ticketname',
     description:
@@ -15,11 +18,25 @@ const mainFeaturedPost = {
 class UseTicket extends Component {
     
     render() {
+    const currentDate = new Date();
+    const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
         return (
-            <div>
+            <div>          
                 <div style={{border: '22px solid white'}}></div>
                 <MainFeaturedPost post={mainFeaturedPost} />
-                test
+                
+                <div style={{border: '2px solid black', textAlign: 'center'}}>
+                <Countdown date={`${year}-12-24T00:00:00`} style={{
+                    display: "flex"}}/>
+                </div>
+
+                <div style={{border: '22px solid white'}}>
+                    <img src={test} alt="what" />
+                </div>
+
+                <div style={{border: '22px solid white'}}>
+                <iframe src="https://open.spotify.com/embed/album/4yP0hdKOZPNshxUOjY0cZj" width="50%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                </div>               
                 test
                 test
                 test
