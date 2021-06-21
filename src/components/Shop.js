02d117e3654a21ea9085ec2cd4de01c1af981ccb
Niklas Denneler <niklas.denneler@gmail.com>
2021-06-22 01:07:17 +0200

    fixed Ticket Shop and rendering of owned Tickets

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
+import Ticket from '../abis/Ticket.json';
+import RecipeReviewCard from './Card.js';
+import differenceBy from 'lodash/differenceBy'
 
 class Shop extends Component {
     state = {
-        available_tickets: []
+        available_tickets: [],
+        contract: '',
+        tickets: [],
+        ownedtickets: [],
+        account: '',
+        availabletickets: []
     }
     
     async loadWeb3() {
@@ -18,7 +27,7 @@ class Shop extends Component {
         }
     }
 
-    async loadBlockchainData() {
+    async allTickets() {
         const web3 = window.web3
         // Load account
         const accounts = await web3.eth.getAccounts()
@@ -36,7 +45,7 @@ class Shop extends Component {
           // Load tickets
           for (var i = 1; i <= totalSupply; i++) {
             const ticket = await contract.methods.metadata(i - 1).call()
-            let ticketobj = new Object({name: ticket.name, artist: ticket.artist, location: ticket.location, price: ticket.price, seat: ticket.seat,date: ticket.date, ipfs_hash: ticket.ipfs_hash});
+            let ticketobj = new Object({name: ticket.name, artist: ticket.artist, location: ticket.location, price: ticket.price, seat: ticket.seat,date: ticket.date, ipfs_hash: ticket.ipfs_hash, id: ticket.id});
             //console.log(ticketobj);
              this.setState({
               tickets: [...this.state.tickets, ticketobj]
@@ -47,12 +56,89 @@ class Shop extends Component {
           window.alert('Smart contract not deployed to detected network.')
         }
       }
+
+      async getTicketsofOwner(){
+        const web3 = window.web3
+        // Load account
+        const accounts = await web3.eth.getAccounts()
+        this.setState({ account: accounts[0] })
+
+        const networkId = await web3.eth.net.getId()
+        const networkData = Ticket.networks[networkId]
+        if(networkData) {
+          const abi = Ticket.abi
+          const address = networkData.address
+          const contract = new web3.eth.Contract(abi, address)
+          this.setState({ contract })
+          const balance = await contract.methods.balanceOf(accounts[0]).call()
+          for ( var i = 0; i < balance.toNumber(); i++) {
+            const tokenid = await contract.methods.tokenOfOwnerByIndex(accounts[0], i).call()
+            const ticket = await contract.methods.getTicketMetadata(tokenid).call()
+           // console.log(ticket);
+            let ownedticketobj = new Object({name: ticket.name, artist: ticket.artist, location: ticket.location, price: ticket.price, seat: ticket.seat,date: ticket.date, ipfs_hash: ticket.ipfs_hash,})
+            this.setState({
+              ownedtickets: [...this.state.ownedtickets, ownedticketobj]
+            })
+            //console.log(this.state.ownedtickets)
+            }
+          }
+         else {
+          window.alert('Smart contract not deployed to detected network.')
+        } 
+      }
+
+      async availableTickets(){
+        //total supply - owned tickets
+        let totalSupply = this.state.tickets
+        let ownedTickets = this.state.ownedtickets
+        const availabletickets = differenceBy(totalSupply, ownedTickets, 'date')
+        this.setState({ availabletickets })
+      }
     
+
+      async transferTicket (ticketid, from, to) {
+        const web3 = window.web3
+       
+        const networkId = await web3.eth.net.getId()
+        const networkData = Ticket.networks[networkId]
+
+        if(networkData) {
+          const abi = Ticket.abi
+          const address = networkData.address
+          const contract = new web3.eth.Contract(abi, address)
+          this.setState({ contract })
+          //transferFrom needs a from address
+          const transfer = await contract.methods.transferFrom(ticketid, from, to)
+        }
+      }
+
+      async componentWillMount() {
+        await this.loadWeb3()
+        await this.allTickets();
+        await this.getTicketsofOwner();
+        await this.availableTickets();
+        
+      }
+
     render() {
 
         return (
             <div>
-                
+                <h5 style={{marginTop:"50px"}}>All Tickets available to purchase:</h5>
+            <div className="row text-center">
+                {/* Show Tickets for User */}
+    
+                <div style={{border: '30px solid white'}}></div>
+                {this.state.availabletickets.map((ticket) => {
+                    return (
+                       
+                        <div className="col-md-3 mb-3">
+                            <div style={{border: '30px solid white'}}></div>
+                            <RecipeReviewCard props={ticket}></RecipeReviewCard>
+                        </div>      
+                    )
+                })}
+            </div>
             </div>
         );
     }
--- src/components/ShowTicket.js
+++ src/components/ShowTicket.js
@@ -7,7 +7,7 @@ import Typography from '@material-ui/core/Typography';
 class ShowTicket extends Component {
     state = {
         tickets: [],
-        ticketsforowner: []
+        ownedtickets: []
     }
 
     async loadWeb3() {
@@ -53,44 +53,45 @@ class ShowTicket extends Component {
         }
       }
 
-    async loadTicketsforOwner(account){
+      async getTicketsofOwner(){
         const web3 = window.web3
+        // Load account
+        const accounts = await web3.eth.getAccounts()
+        this.setState({ account: accounts[0] })
+
         const networkId = await web3.eth.net.getId()
         const networkData = Ticket.networks[networkId]
-        if (networkData){
-        const abi = Ticket.abi
-        const address = networkData.address
-        const contract = new web3.eth.Contract(abi, address)
-        this.setState({ contract })
-        const totalSupplyforOwner = await contract.methods.balanceOf(account).call()
-        this.setState({ totalSupplyforOwner })
-        // Load tickets
-        for (var i = 1; i <= totalSupplyforOwner; i++) {
-          const ticket = await contract.methods.metadata(i - 1).call()
-          let ticketobjforowner = new Object({name: ticket.name, artist: ticket.artist, location: ticket.location, price: ticket.price, seat: ticket.seat,date: ticket.date, ipfs_hash: ticket.ipfs_hash});
-          console.log(ticketobjforowner);
-           this.setState({
-            ticketsforowner: [...this.state.ticketsforowner, ticketobjforowner]
-          })
-          //console.log(this.state.tickets);
-        }
-      } else {
-        window.alert('Error Loading Tickets by Owner')
+        if(networkData) {
+          const abi = Ticket.abi
+          const address = networkData.address
+          const contract = new web3.eth.Contract(abi, address)
+          this.setState({ contract })
+          const balance = await contract.methods.balanceOf(accounts[0]).call()
+          for ( var i = 0; i < balance.toNumber(); i++) {
+            const tokenid = await contract.methods.tokenOfOwnerByIndex(accounts[0], i).call()
+            const ticket = await contract.methods.getTicketMetadata(tokenid).call()
+            let ownedticketobj = new Object({name: ticket.name, artist: ticket.artist, location: ticket.location, price: ticket.price, seat: ticket.seat,date: ticket.date, ipfs_hash: ticket.ipfs_hash,})
+            this.setState({
+              tickets: [...this.state.ownedtickets, ownedticketobj]
+            })
+            }
+          }
+         else {
+          window.alert('Smart contract not deployed to detected network.')
+        } 
       }
-    }
     
 
     async componentWillMount() {
         await this.loadWeb3();
-        await this.loadBlockchainData();
-        await this.loadTicketsforOwner(this.state.account);
+        await this.getTicketsofOwner();
       }
 
     render() {
         return (
             <div>
             <h5 style={{marginTop:"50px"}}>
-                All Tickets available to purchase:
+                Your tickets:
             </h5>
             <div className="row text-center">
             
@@ -105,21 +106,7 @@ class ShowTicket extends Component {
                     )
                 })}
             </div>   
-            <h5>Owned Tickets:</h5>
-            <div className="row text-center">
-                {/* Show Tickets for User */}
-    
-                <div style={{border: '30px solid white'}}></div>
-                {this.state.ticketsforowner.map((ticket) => {
-                    return (
-                       
-                        <div className="col-md-3 mb-3">
-                            <div style={{border: '30px solid white'}}></div>
-                            <RecipeReviewCard props={ticket}></RecipeReviewCard>
-                        </div>      
-                    )
-                })}
-            </div>
+            
             </div>
         );
     }
